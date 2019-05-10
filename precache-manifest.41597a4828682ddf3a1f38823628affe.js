721cf44903983c2cb10d62e7e7eca7ecbca6db28
maribel-arac <maribel.arca@gmail.com>
2019-05-10 13:07:25 -0500

    Updates

---
+++ precache-manifest.41597a4828682ddf3a1f38823628affe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e412534393edfb5a15e9e12663de43b8",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "8725781fe58c8a34b101",
+    "url": "/GDL002-burger-queen/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL002-burger-queen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8725781fe58c8a34b101",
+    "url": "/GDL002-burger-queen/static/js/main.adccf0f9.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
