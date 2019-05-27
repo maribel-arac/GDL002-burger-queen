746ac566090070293ee06d84d36ceb0570ce8471
maribel-arac <maribel.arca@gmail.com>
2019-05-27 16:50:27 -0500

    the delete icon is included and works

--- burger-queen/src/components/orders.js
+++ burger-queen/src/components/orders.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-export class Order extends React.Component{
+export class Order extends React.Component {
+
+    deleteItem = (e, i) => {
+        this.props.handleDelete(e, i)
+    }
+
     render(){
         const orders = this.props.menuOrder.map((menu, i) => {
             return(
@@ -9,6 +14,7 @@ export class Order extends React.Component{
                         <li className="list-group-item d-flex justify-content-between align-items-center col-md-12">
                             <p> {menu.item + " "+ menu.amount}</p>
                             <span className="btn btn-success">{"$"+ menu.price}</span>
+                            <button id={i} onClick={(event)=> this.deleteItem(event, menu)}><i className=" btn btn-danger fas fa-trash-alt"></i></button>
                         </li>
                     </ul>
                 </div>
