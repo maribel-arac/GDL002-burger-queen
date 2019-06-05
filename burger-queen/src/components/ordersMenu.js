5d81b97b1714a6523f380facda3cb346d77f62d6
maribel-arac <maribel.arca@gmail.com>
2019-06-05 18:51:49 -0500

    change the name from menu tu ordersMenu

---
+++ burger-queen/src/components/ordersMenu.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+export class OrderMenu extends React.Component {
+
+    deleteItem = (e, i) => {
+        this.props.handleDelete(e, i)
+    }
+
+    render(){
+        const orders = this.props.menuOrder.map((menu, i) => {
+            return(
+                <div>
+                    <ul key={i} className="list-group col-md-12">
+                        <li className="list-group-item d-flex justify-content-between align-items-center col-md-12">
+                            <p> {menu.item + " "+ menu.amount}</p>
+                            <span className="btn btn-success">{"$"+ menu.price}</span>
+                            <button id={i} onClick={(event)=> this.deleteItem(event, menu)}><i className=" btn btn-danger fas fa-trash-alt"></i></button>
+                     
+                        </li>
+                    </ul>
+                </div>
+            )
+        })
+        return(
+            orders
+
+        )
+    }
+}
+
+export default OrderMenu;
